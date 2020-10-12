045f80b48502a82625f383bcb1634a658cbed719
{SakaeShinya} <sakashin10291029@gmail.com>
2020-10-12 20:38:56 +0900

    コメント追加

--- public/src/interface.ts
+++ public/src/interface.ts
@@ -1,16 +1,20 @@
+//Point class
 export interface Pointable {
     readonly totalpoint: number;
 }
+//Karma class
 export interface Karmaable {
     element: HTMLDivElement;
     clickEventHandler(): void;
 }
+//Karmas class
 export interface Karmasable {
     elements: NodeListOf<HTMLDivElement>;
     readonly activeElements: HTMLDivElement[];
     readonly activeElementspoint: number[];
     remove(): void;
 }
+//Judge class
 export interface Judgeable {
     element: HTMLDivElement;
     readonly resultPoint: number;
